3da0bb064ecf214a5eef156641916e066ad33b32
Aqib Khan <139586361+AqibKhan61@users.noreply.github.com>
2024-02-14 11:21:25 +0500

    Added a TextField soo User can enter any Number

--- lib/sms.dart
+++ lib/sms.dart
@@ -3,9 +3,15 @@ import 'package:url_launcher/url_launcher.dart';
 
 
 
-class SMS extends StatelessWidget{
+class SMS extends StatefulWidget{
   const SMS({super.key});
 
+  @override
+  State<SMS> createState() => _SMSState();
+}
+
+class _SMSState extends State<SMS> {
+  String number = '';
   void sendSms(String phoneNumber, String message) async {
   final Uri uri = Uri(
     scheme: 'sms',
@@ -19,21 +25,50 @@ class SMS extends StatelessWidget{
     throw 'Could not launch SMS';
   }
 }
+
   @override
   Widget build(BuildContext context) {
+    final TextEditingController controller = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: const Text('SMS Sender'),
         centerTitle: true,
       ),
-      body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              sendSms('923111687177', 'Check out our web app: https://www.google.com');
-            },
-            child: const Text('Send SMS'),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 15, right: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: TextField(
+                controller: controller,
+                keyboardType: TextInputType.number,
+                onEditingComplete: (){
+                  setState(() {
+                    number = controller.text;
+                  });
+                },
+                decoration: InputDecoration(
+                  hintText: 'Enter your Number',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  )
+                ),
+              ),
+            ),
+            const SizedBox(height: 15,),
+            Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    sendSms(number, 'Check out our web app: https://www.google.com');
+                  },
+                  child: const Text('Send SMS'),
+                ),
+              ),
+          ],
         ),
+      ),
     );
   }
 }
